89c8d674f404d8090a10f6ec209173920d377932
mikabe1805 <mikushbe@gmail.com>
2023-09-19 11:45:47 -0400

    more experiments

--- index.php
+++ index.php
@@ -12,15 +12,17 @@
         </style>"
         ?>
         <?php
-        	$url = "style.css";
+        	$url = "<css>style.css";
         	echo "<link rel='stylesheet' href='{$stylesheet_url}'>";
         ?>
+        <?php echo file_get_contents("html/header.html"); ?>
     </head>
     <body>
         <h1 <?php echo "style='color:blue'" ?>>Hello, world!</h1>
+        <?php echo file_get_contents("html/body.html"); ?>
     </body>
+    <footer>
+    <?php echo file_get_contents("html/footer.html"); ?>
+</footer>
 </html>
-<?php echo file_get_contents("html/header.html"); ?>
-<?php echo file_get_contents("html/body.html"); ?>
-<?php echo file_get_contents("html/footer.html"); ?>
 <p>Copyright &copy; Mika Be Abi Pyatski and Christina Drachuk <?php echo date("Y"); ?></p>
