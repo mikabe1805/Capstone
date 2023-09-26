4b2ad034bb62efac3965ca009881ac841d95dbcf
mikabe1805 <mikushbe@gmail.com>
2023-09-26 09:22:49 -0400

    fun

--- index.php
+++ index.php
@@ -56,8 +56,29 @@
     </form> -->
   </div>
 </nav>
-
-
+    <section class="my-4">
+        <div class="py-4">
+            <h2 class="text-center">Contact Us</h2>
+        </div>
+        <div class="w-50 m-auto">
+            <form action="about.php" method="post">
+                <div class="form-group">
+                    <label>Name:</label>
+                    <input type="text" name="name" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Email:</label>
+                    <input type="text" name="email" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Message:</label>
+                    <input type="text" name="message" class="form-control">
+                </div>
+                <button type="submit" class="btn btn-success">Submit</button>
+            </form>
+        </div>
+    </section>
+    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
